57ac4a4c7144da2a29a2244928d57d1ea8f0f9d8
Philip Martin <pam79@biosci.cam.ac.uk>
2019-06-27 13:58:47 +0100

    added download of data from online sources, tweaked figures, and removed code that is no longer needed - May need to modify map as it is not displaying correctly at present

--- code/combined_processing_and_analysis_for_katia.R
+++ code/combined_processing_and_analysis_for_katia.R
@@ -63,6 +63,7 @@ library(spatialEco)
 library(cowplot)
 library(viridis)
 library(snow)
+library(rgeos)
 
 
 beginCluster(n=6,type="SOCK") 
@@ -72,7 +73,24 @@ beginCluster(n=6,type="SOCK")
 ###################################################################
 
 BII_map<-raster("data/lbii.asc")#load in BII map data
+
+#download biomass data from Erb et al 2017 https://doi.org/10.1038/nature25138
+download.file("https://boku.ac.at/fileadmin/data/H03000/H73000/H73700/Data_Download/Data/NatureDataDownload.zip", 
+              destfile = 'NatureDataDownload.zip')
+unzip(zipfile = "data/NatureDataDownload.zip",#unzip biomass dataset 
+      exdir = 'data')
+unzip(zipfile = "data/NATURE_DATADOWNLOAD.zip",#unzip biomass dataset 
+      exdir = 'data')
+file.rename(from="data/NEW/C_reduction_perc.tif",to="data/C_reduction_perc.tif")#move file from subdirectory
+unlink("data/NEW/",recursive = TRUE)#remove biomass folder with unwanted files
+file.remove("data/NATURE_DATADOWNLOAD.zip")#remove biomass zips with unwanted files
+file.remove("data/NatureDataDownload.zip")#remove biomass zips with unwanted files
 biomass<-raster("data/C_reduction_perc.tif")#load in biomass map
+
+#download human footprint data from Venter et al 2015 https://doi.org/10.1038/sdata.2016.67 (data location http://dx.doi.org/10.5061/dryad.052q5)
+download.file("https://datadryad.org/bitstream/handle/10255/dryad.131447/HumanFootprintv2.7z?sequence=2", 
+              destfile = 'data/HumanFootprintv2.7z')
+
 hfp<-raster("data/HFP2009.tif")
 
 
@@ -93,16 +111,17 @@ BII_proj <- projectRaster(BII_agg, BII_ex)
 writeRaster(BII_proj, "data/BII_moll", format = "GTiff")#save projected BII
 BII_proj_capped<-BII_proj
 values(BII_proj_capped)[values(BII_proj_capped) >=1] = 1#set maximum BII value as 1
-writeRaster(BII_proj_capped, "data/BII_corrected", format = "GTiff",overwrite=T) #save BII map with max value of 1
+writeRaster(BII_proj_capped, "data/BII_capped", format = "GTiff",overwrite=T) #save BII map with max value of 1
 
-#create grid to extract data to
-r <- raster(extent(biomass), nrow=2169, ncol=4337, #create a grid with resolution 0.083 degrees to extract data to
+#create a grid to extract data to with a resolution of 0.083333 degrees
+r <- raster(extent(biomass), nrow=2169, ncol=4337,
             crs = "+proj=moll +lon_0=0 +ellps=WGS84 +units=m +no_defs")
 r[] <- 1:ncell(r)
 sp.r <- as(r, "SpatialPixelsDataFrame")
 
+
 sp.r$biomass<-(extract(biomass_inv,sp.r))#extract values from biomass raster
-sp.r$bii<-(extract(BII_proj,sp.r))#extract values from BII data
+sp.r$bii<-(extract(BII_proj_capped,sp.r))#extract values from BII data
 spr_df<-as.data.frame(sp.r)#convert grid to a dataframe for plotting in  ggplot
 write.csv(spr_df,"data/output_data//BII_BMI_data.csv")#save this data as a csv file
 head(spr_df)
@@ -160,37 +179,40 @@ write.csv(ecoregion_hotspot_stats,"data/output_data/ecoregion_hotspot_stats.csv"
 #####################################################################
 
 spr_df<-read.csv("data/output_data/BII_BMI_data.csv")#read in data extracted to grid
-summary(spr_df)#summary of data extracted to grid
-mean(spr_df$bii,na.rm=T)
+spr_df_cc<-spr_df[complete.cases(spr_df),]#remove rows with missing data
 
-spr_df_cc<-spr_df[complete.cases(spr_df), ]#remove rows with missing data
+#download coastline dataset
+download.file("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_coastline.zip", 
+              destfile = 'coastlines.zip')
+unzip(zipfile = "coastlines.zip",#unzip coastline dataset 
+      exdir = 'data')
+coast<-readOGR("data/ne_10m_coastline.shp")
+coast_moll<- spTransform(coast, crs(biomass_inv))
+coast_simp<-gSimplify(coast,tol = 0.1,topologyPreserve = TRUE)#simplify coastline to speed up plotting
 
-
-coast<-readOGR(dsn="data/coastline/ne_10m_coastline.shp",layer="ne_10m_coastline")#load in coastline dataset
-coast@data$id<-rownames(coast@data)#fortify coastline shapefile for plotting in ggplot
-coast.points<-fortify(coast,region="id")
+coast_df<-SpatialLinesDataFrame(coast_simp,coast@data) 
 
 #plot bivariate map of biomass intactness vs biodiversity intactness
 #alter function 'col_func' to change the tones used in the map
 biv_map1<-ggplot()
-biv_map2<-biv_map1+geom_raster(data=spr_df,aes(x=x,y=y,fill=biomass,fill2=bii))+
+biv_map2<-biv_map1+geom_tile(data=spr_df,aes(x=x,y=y,fill=biomass,fill2=bii))+
          scale_fill_colourplane(name = "",na.color = "NA",color_projection = col_func,
          limits_y = c(0,1),limits=c(0,1),axis_title = "biomass \nintactness",
          axis_title_y = "Biodiversity\n Intactness \nIndex",breaks = c(0,0.5,1),breaks_y = c(0,0.5,1),
          hue1 = 1, hue2 = 0.62,
          dark_grey = 0.5,
          light_grey = 0.9)+
-         theme_bw()+coord_equal(xlim = c(-180,180),ylim = c(-50,80))+
+         theme_bw()+coord_equal()+
+         guides(fill = guide_colorplane(title.theme = element_text(size = 13),label.theme = theme_gray(),
+         label.y.theme = theme_gray(),axis.ticks = element_blank()))+
          theme(axis.line=element_blank(),panel.background=element_blank(),panel.border=element_blank(),
-         panel.grid.major=element_blank(),panel.grid.minor=element_blank(),plot.background=element_blank())+
-         guides(fill = guide_colorplane(title.theme = element_text(size = 13),label.theme = theme_gray(),label.y.theme = theme_gray()))+
-         theme(legend.position=c(.1,.3),panel.background = element_blank(),axis.text = element_blank(),axis.ticks = element_blank(),legend.key.size=unit(0.8,"cm"))
-biv_map3<-biv_map2+geom_path(data=coast.points,aes(x = long, y = lat, group = group),colour="black",size=0.1)
+         panel.grid.major=element_blank(),panel.grid.minor=element_blank(),plot.background=element_blank(),
+         axis.ticks = element_blank(),axis.text = element_blank(),axis.title = element_blank(),
+         legend.position=c(.1,.3),legend.key.size=unit(0.8,"cm"))
+biv_map2
 
 
-hotspot_overlap_df<-read.csv("data/hotspot_overlap.csv")#load ecoregion statistics
-
-
+hotspot_overlap_df<-read.csv("data/output_data/ecoregion_hotspot_stats.csv")#load ecoregion statistics
 #calculate median and quartiles for BII and BMI for ecoregions with >50% overlap with hotspots
 #and ecoregions with <50% overlap with hotspots
 hotspot_median<-ddply(hotspot_overlap_df,.(hotspot),  
@@ -207,31 +229,29 @@ hotspot_median<-ddply(hotspot_overlap_df,.(hotspot),
 hs_BMI_BII_1<-ggplot(hotspot_overlap_df,aes(x=biomass,y=bii,colour=hotspot))+
   geom_point(shape=16,alpha=0.4)+
   geom_abline(lty=2)+coord_cartesian(xlim=c(0,1.1),ylim=c(0,1.1),expand = F)
+
 hs_BMI_BII_2<-hs_BMI_BII_1+scale_color_manual("",values=c("hotspot"="red","not hotspot"="dark grey"))+
   ylab("Biodiversity Intactness Index")+
   xlab("biomass intactness")
+
 hs_BMI_BII_3<-hs_BMI_BII_2+geom_point(data=hotspot_median,aes(x=m_biomass,y=m_bii,
              colour=hotspot),shape=15,size=2,stroke=1)+theme(legend.position="none")
+
 hs_BMI_BII_4<-hs_BMI_BII_3+geom_errorbarh(data=hotspot_median,aes(x=m_biomass,y=m_bii,
               xmin=biom_25,xmax=biom_75,colour=hotspot),size=0.5,height=0.005)+
   geom_errorbar(data=hotspot_median,aes(x=m_biomass,y=m_bii,
   ymin=bii_25,ymax=bii_75,colour=hotspot),size=0.5,width=0.005)
-save_plot(hs_BMI_BII_4,filename = "figures/ecoregion_scatter_line_2.png",base_aspect_ratio = 1.2,dpi=600)
-
 
 #plot hf vs bii for ecoregions
 hf_bii_plot1<-ggplot(hotspot_overlap_df,aes(x=hf,y=bii,colour=hotspot))+geom_point(shape=1,alpha=0.8)
 hf_bii_plot2<-hf_bii_plot1+scale_color_manual("",values=c("hotspot"="red","not hotspot"="dark grey"))+ylab("Biodiversity Intactness Index")+xlab("Human Footprint")
 hf_bii_plot3<-hf_bii_plot2+theme(legend.position="none")
-save_plot(hf_bii_plot3,filename = "figures/hf_bii_scatter.png",base_aspect_ratio = 1.2,dpi=600)
 
 #plot hf vs biomass for ecoregions
 hf_bmi_plot1<-ggplot(hotspot_overlap_df,aes(x=hf,y=biomass,colour=hotspot))+geom_point(shape=1,alpha=0.8)
 hf_bmi_plot2<-hf_bmi_plot1+scale_color_manual("",values=c("hotspot"="red","not hotspot"="dark grey"))+ylab("biomass intactness")+xlab("Human Footprint")
 hf_bmi_plot3<-hf_bmi_plot2+theme(legend.position="none")
-save_plot(hf_bmi_plot3,filename = "figures/hf_bmi_scatter.png",base_aspect_ratio = 1.2,dpi=600)
 
-
-bottom_row<-plot_grid(hf_bmi_plot3,hf_bii_plot3,hot_plot4_v2,labels=c("b","c","d"),align='h',ncol=3)
-combined_plots<-plot_grid(biv_map3,bottom_row,labels=c('a',''),ncol=1,rel_heights=c(1.6,1))
+bottom_row<-plot_grid(hf_bmi_plot3,hf_bii_plot3,hs_BMI_BII_4,labels=c("b","c","d"),align='h',ncol=3)
+combined_plots<-plot_grid(biv_map2,bottom_row,labels=c('a',''),ncol=1,rel_heights=c(1.6,1))
 save_plot(combined_plots,filename = "figures/combined_plot_new.png",base_aspect_ratio = 1.2,dpi=600,base_height = 10,base_width = 12)
